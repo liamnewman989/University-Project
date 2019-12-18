4ebd9e375cb1d9a9fc90d1a3ba53c86eac4ab37a
liamnewman989 <51636075+liamnewman989@users.noreply.github.com>
2019-12-18 11:51:33 +0330

    Delete 3_init_resource.js

--- migrations/3_init_resource.js
+++
@@ -1,5 +0,0 @@
-const Resource = artifacts.require("Resource");
-module.exports = function (deployer) {
-  // Use deployer to state migration tasks.
-  deployer.deploy(Resource, "", "", "0x0000000000000000000000000000000000000000");
-};
